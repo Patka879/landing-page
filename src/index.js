3c5d200a24c267ed34f15c52f06bc94c27c020f3
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-09-08 17:45:31 +0200

    header created, header swiper added

--- src/index.js
+++ src/index.js
@@ -11,34 +11,11 @@ const discountSwiper = new Swiper('.swiper', {
   
     autoplay: {
       delay: 2500,
-      disableOnInteraction: false,
+      disableOnInteraction: true,
       pauseOnMouseEnter: true,
     },
   });
   
-  const appSwiper = new Swiper('.app-swiper', {
-    loop: true,
-    simulateTouch: true,
-  
-    autoplay: {
-      delay: 2500,
-      disableOnInteraction: false,
-      pauseOnMouseEnter: true,
-  
-    },
-  
-    slidesPerView: 3,
-    spaceBetween: 5,
-  
-    breakpoints: {
-      1300: {
-        slidesPerView: 5,
-        autoplay: false,
-      }
-    }
-  
-  })
-    
   document.querySelector(".dropdown-menu-btn").addEventListener("click", function(event) {
     event.preventDefault()
     let nav = document.querySelector(".nav-top")
