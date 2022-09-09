aca5cf0a711378d85d1b94c8844afab13ba58a64
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-09-09 15:32:47 +0200

    background image created for form-sections. smooth scrolling to anchor tags added, animation on scroll effect added

--- src/index.js
+++ src/index.js
@@ -37,3 +37,30 @@ document.querySelector(".ok-btn").addEventListener("click", function(event) {
     document.querySelector(".overlay").style.display = "none";
     document.querySelector(".form-overlay").style.display = "none";
 })
+
+function reveal() {
+    let reveals = document.querySelectorAll(".facts-section, .innovation-section, .gallery-section, .form-section");
+    for (let i = 0; i < reveals.length; i++) {
+      let windowHeight = window.innerHeight;
+      let elementTop = reveals[i].getBoundingClientRect().top;
+      let elementVisible = 150;
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+
+window.addEventListener("scroll", reveal);
+reveal();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
