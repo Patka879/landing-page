584b7825da90e437ff7b729b1d63f11db80642f5
Patrycja Mysiak <mysiak.patrycja@gmail.com>
2022-09-09 14:28:41 +0200

    overlay and overlay rules created

--- src/index.js
+++ src/index.js
@@ -16,12 +16,24 @@ const discountSwiper = new Swiper('.swiper', {
     },
   });
   
-  document.querySelector(".dropdown-menu-btn").addEventListener("click", function(event) {
+//   document.querySelector(".dropdown-menu-btn").addEventListener("click", function(event) {
+//     event.preventDefault()
+//     let nav = document.querySelector(".nav-top")
+//     if (nav.classList.contains("nav-top-visible")) {
+//       nav.classList.remove("nav-top-visible")
+//     } else {
+//       nav.classList.add("nav-top-visible")
+//     }
+//   })
+
+document.querySelector(".submit-btn").addEventListener("click", function(event) {
     event.preventDefault()
-    let nav = document.querySelector(".nav-top")
-    if (nav.classList.contains("nav-top-visible")) {
-      nav.classList.remove("nav-top-visible")
-    } else {
-      nav.classList.add("nav-top-visible")
-    }
-  })
+    document.querySelector(".overlay").style.display = "block";
+    document.querySelector(".form-overlay").style.display = "block";
+})
+
+document.querySelector(".ok-btn").addEventListener("click", function(event) {
+    event.preventDefault()
+    document.querySelector(".overlay").style.display = "none";
+    document.querySelector(".form-overlay").style.display = "none";
+})
